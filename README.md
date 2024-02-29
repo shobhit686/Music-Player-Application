78a1fe93ed41b4fb9a2376012970ce86c932cff8
Shobhit Sharma <shobhitsharma686@gmail.com>
2024-03-01 02:33:24 +0530

    Initial local commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Music-Player-Application
+# Welcome to Music-Player-Application Project
+This project is a clone design of Spotify.com but with changes in its features, allowing it to play songs on the landing page. The project's design is responsive and is coded using best practices. All the features included respond well.
+
+### Screenshots
+![Site Image](<assets/Readme utilities/scr1.png>)
+
+### How to Setup and Run the Project
+    1. Clone or download the repository in your local system. Please ensure that the file structure remains the same.
+    2. Use any browser to render the HTML
+
+### Dependencies
+There are no dependencies for this project. Only requirement is to have a modern internet browser that can serve HTML files
+
+### Tech Stack
+Vanilla JavaScript, HTML, CSS
+
+### I do not claim to own the images used in the project and they are property of their respective owners 
---
+++ js/script.js
@@ -0,0 +1,147 @@
+// console.log("Beginning of JS")
+let currentSong = new Audio();
+let songs;
+let currFolder;
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+//use folder to pass as argument in function
+async function getSongs(folder) {
+    currFolder = folder + "/"
+    console.log(currFolder)
+    let a = await fetch(`http://127.0.0.1:3000/Spotify-clone/${folder}`);
+    // let a = await fetch("http://127.0.0.1:3000/Spotify-clone/songs/ncs/");
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    // console.log(as)
+    songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1]);
+            // songs.push(element.href.split(`/ncs/`)[1]);
+        }
+    }
+    // console.log(songs)
+    return songs;
+}
+
+const playMusic = (track, pause=false)=>{
+    currentSong.src = `http://127.0.0.1:3000/Spotify-clone/${currFolder}` + track;
+    // currentSong.src = "http://127.0.0.1:3000/Spotify-clone/songs/ncs/" + track;
+    console.log(currentSong.src)
+    if(!pause){
+        currentSong.play()
+        play.src = "assets/images/pause.svg"
+    }
+    document.querySelector(".song-info").innerHTML = decodeURI(track)
+    document.querySelector(".song-duration").innerHTML = "00:00/00:00"
+}
+
+async function main() {
+
+    // Get list of all the songs
+    songs = await getSongs("songs/ncs");
+    // songs = await getSongs();
+    playMusic(songs[0], true)
+    console.log(songs);
+
+    let songUL = document.querySelector(".song-list").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+        <img class="invert" src="assets/images/music.svg" alt="music icon">
+        <div class="info">
+            <div> ${song.replaceAll("%20", " ")}</div>
+            <div> Artist </div>
+        </div>
+        <div class="playnow">
+            <span>Play Now</span>
+            <img class="invert" src="assets/images/play.svg" alt="play btn">
+        </div>
+    </li>`;
+    }
+    console.log(songUL)
+
+    let audio = new Audio(songs[0]);
+    // audio.play();
+
+    //Attach event listener to play any song from list
+    Array.from(document.querySelector(".song-list").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML.trim())
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    });
+
+    //Attach event listener to play, pause, prev and next from buttons
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "assets/images/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "assets/images/play.svg"
+        }
+    })
+
+    //Timeupdate listener
+    currentSong.addEventListener("timeupdate", ()=>{
+        // console.log(currentSong.currentTime, currentSong.duration)
+        document.querySelector(".song-duration").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration)*100 + "%";
+    })
+
+    //Seekbar control listener
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left = percent + "%"
+        currentSong.currentTime = (currentSong.duration*percent)/100
+    })
+
+    //Listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = 0;
+    })
+    
+    //Listener for close button
+    document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "-120%";
+    })
+
+    //Listener for prev button
+    previous.addEventListener("click", ()=>{
+        // console.log("Prev clicked")
+        let index = songs.indexOf(currentSong.src.split("/ncs/").slice(-1)[0])
+        if((index-1)>=0)
+            playMusic(songs[index-1])
+        // console.log(songs, index)
+    })
+
+    //Listener for next button
+    next.addEventListener("click", ()=>{
+        // console.log("Next clicked")
+        // console.log(currentSong.src)
+        let index = songs.indexOf(currentSong.src.split("/ncs/").slice(-1)[0])
+        if((index+1)< songs.length)
+            playMusic(songs[index+1])
+        // console.log(songs, index)
+    })
+}
+
+main()
